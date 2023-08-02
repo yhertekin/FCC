76e340dad09da154061e73a24414955696be55ad
yhertekin <yhertekin@gmail.com>
2023-08-02 22:19:10 +0300

    add roman numeral converter

---
+++ JavaScript Algorithms and Data Structures/Roman Numeral Converter/app.js
@@ -0,0 +1,30 @@
+function convertToRoman(number) {
+	const romanNumerals = [
+		{ value: 1000, numeral: "M" },
+		{ value: 900, numeral: "CM" },
+		{ value: 500, numeral: "D" },
+		{ value: 400, numeral: "CD" },
+		{ value: 100, numeral: "C" },
+		{ value: 90, numeral: "XC" },
+		{ value: 50, numeral: "L" },
+		{ value: 40, numeral: "XL" },
+		{ value: 10, numeral: "X" },
+		{ value: 9, numeral: "IX" },
+		{ value: 5, numeral: "V" },
+		{ value: 4, numeral: "IV" },
+		{ value: 1, numeral: "I" },
+	];
+
+	let result = "";
+
+	for (const numeral of romanNumerals) {
+		while (number >= numeral.value) {
+			result += numeral.numeral;
+			number -= numeral.value;
+		}
+	}
+
+	return result;
+}
+
+convertToRoman(3000);
